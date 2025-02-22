f7b94e0e372ae441999f2325761f2a6075eb1cf0
majsterbobo <borowy.se@gmail.com>
2025-02-22 21:51:54 +0000

    startof

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+#define WIDTH 900
+#define HEIGHT 600
+#define COLOR_WHITE 0xffffffff
+
+int main(){
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("RayTracing", SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+    SDL_Rect rect = (SDL_Rect){200, 200, 200, 200};
+    SDL_FillRect(surface, &rect, COLOR_WHITE);
+    SDL_UpdateWindowSurface(window);
+    
+    
+    SDL_Delay(5000);
+
+}
